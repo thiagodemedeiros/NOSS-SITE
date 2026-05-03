7e4f88cb4df339ef9cfffc5ab8c35302c0489eec
Thiago Medeiros <thiagotitico1@gmail.com>
2026-05-03 19:46:19 -0300

    Corrigindo bug da secao 2

--- src/feature/Home/Home.tsx
+++ src/feature/Home/Home.tsx
@@ -1,11 +1,13 @@
 import HomeSection1 from "./Sections/HomeSection1";
 import HomeSection2 from "./Sections/HomeSection2";
+import HomeSection3 from "./Sections/HomeSection3";
 
 export default function Home() {
     return(
         <>
             <HomeSection1/>
             <HomeSection2/>
+            <HomeSection3/>
         </>
     )
 };
---
+++ src/feature/Home/Sections/HomeSection3.tsx
@@ -0,0 +1,12 @@
+import "./styles/HomeSection3.css"
+
+export default function HomeSection3() {
+    return(
+        <section className="HomeSection3">
+            <div className="HomeSection3Content">
+                <h4>30.05.2026</h4>
+                <p>É a data do nosso encontro</p>
+            </div>
+        </section>
+    )
+}
