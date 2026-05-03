7ac65115610557194ac665acc328f637f50860ae
Thiago Medeiros <thiagotitico1@gmail.com>
2026-05-03 16:46:46 -0300

    Primeira seção pronto

--- src/feature/Home/Sections/HomeSection1.tsx
+++ src/feature/Home/Sections/HomeSection1.tsx
@@ -1,9 +1,10 @@
 import "./styles/HomeSection1.css";
+import logo from "./assets/home.section1.logo.png";
 
 export default function HomeSection1() {
     return(
         <section className="HomeSection1">
-            <h1>NOSS</h1>
+            <img src={logo} alt="NOSS Logo" />
         </section>
     )
 };
