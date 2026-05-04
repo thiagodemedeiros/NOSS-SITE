ccfec3406dc76552604f9f3f8133ac5033696cbc
Thiago Medeiros <thiagotitico1@gmail.com>
2026-05-03 21:29:38 -0300

    Secao 5 pronta

--- src/feature/Home/Home.tsx
+++ src/feature/Home/Home.tsx
@@ -2,6 +2,7 @@ import HomeSection1 from "./Sections/HomeSection1";
 import HomeSection2 from "./Sections/HomeSection2";
 import HomeSection3 from "./Sections/HomeSection3";
 import HomeSection4 from "./Sections/HomeSection4";
+import HomeSection5 from "./Sections/HomeSection5";
 
 export default function Home() {
     return(
@@ -10,6 +11,7 @@ export default function Home() {
             <HomeSection2/>
             <HomeSection3/>
             <HomeSection4/>
+            <HomeSection5/>
         </>
     )
 };
---
+++ src/feature/Home/Sections/HomeSection5.tsx
@@ -0,0 +1,22 @@
+import "./styles/HomeSection5.css"
+import githubLogo from "./assets/home.section5.githubLogo.png"
+
+const linkGithub = "https://github.com/cumbucadev/NOSS/tree/main";
+
+export default function HomeSection5() {
+    return (
+        <section className="HomeSection5">
+            <div className="HomeSection5Content">
+                <a href={linkGithub} className="HomeSection5Content_Github">
+                    <img src={githubLogo} alt="GitHub Logo" />
+                    <h4>/@CumbucaDev</h4>
+                </a>
+                <a href={linkGithub} className="HomeSection5Content_Button">
+                    <button>
+                        Contribua!!!
+                    </button>
+                </a>
+            </div>
+        </section>
+    )
+}
