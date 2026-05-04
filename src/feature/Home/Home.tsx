854740644e19f2a1f14d585979d4019c09d24743
Thiago Medeiros <thiagotitico1@gmail.com>
2026-05-03 21:21:44 -0300

    Secaop 4 pronta

--- src/feature/Home/Home.tsx
+++ src/feature/Home/Home.tsx
@@ -1,6 +1,7 @@
 import HomeSection1 from "./Sections/HomeSection1";
 import HomeSection2 from "./Sections/HomeSection2";
 import HomeSection3 from "./Sections/HomeSection3";
+import HomeSection4 from "./Sections/HomeSection4";
 
 export default function Home() {
     return(
@@ -8,6 +9,7 @@ export default function Home() {
             <HomeSection1/>
             <HomeSection2/>
             <HomeSection3/>
+            <HomeSection4/>
         </>
     )
 };
---
+++ src/feature/Home/Sections/HomeSection4.tsx
@@ -0,0 +1,20 @@
+import "./styles/HomeSection4.css"
+import youtubeLogo from "./assets/home.section4.youtubelogo.png"
+
+export default function HomeSection4() {
+    return (
+        <section className="HomeSection4">
+            <div className="HomeSection4Content">
+                <a href="https://www.youtube.com/@CumbucaDev" className="HomeSection4Content_Youtube">
+                    <img src={youtubeLogo} alt="YouTube Logo" />
+                    <h4>/@CumbucaDev</h4>
+                </a>
+                <a href="https://www.youtube.com/@CumbucaDev" className="HomeSection4Content_Button">
+                    <button>
+                        Inscreva-se
+                    </button>
+                </a>
+            </div>
+        </section>
+    )
+}
